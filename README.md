41fadd536ebbaff42ca423961b147d0f8abaedf2
Matúš Mrázik <matus.mrazik2@gmail.com>
2022-10-17 19:45:08 +0200

    Added eslint, removed some errors

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Homework
+# Todo app
+
+## **Original assignment**
 
 Here is sample code that is not by far ideal.
 
@@ -9,3 +11,10 @@ Here is sample code that is not by far ideal.
 5. Add Todo detail page (add routing to app, use context api for state managment from fetch todos to render todos and detail)
 6. Todo component has defined shouldComponentUpdate lifecycle, but it can be done better, adjust it
 7. Optional: rewrite Todo component to FC (choose if you want, prepare explanation)
+
+## **My solution**
+
+First, I added eslint for code style and errors checking. I also added prettier for code formatting with rules that I usually use. The problems I found are:
+
+1. In `App.tsx`, when rendering the `Todo` components, there is no `key` prop defined for the todo items.
+2. In some places, `var` was used to declare variables, which is an old way of declaring variables, `let` and `const` are safer and can reduce runtime errors.
--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,46 @@
-import React from "react";
-
-import Todo from './Todo';
+import React from 'react'
+import Todo from './Todo'
 
 // rework this into regular api call, feel free to use any open api
-var todos = (): Promise<{id: string; title: string;}[]> => new Promise((res) => {
-  setTimeout(() => {
-    res([
-      {
-        id: "1",
-        title: "Go shopping",
-      },
-      {
-        id: "2",
-        title: "Job interview",
-      },
-      {
-        id: "3",
-        title: "Prepare homework",
-      },
-    ]);
-  }, 100);
-});
+const todos = (): Promise<{ id: string; title: string }[]> =>
+  new Promise(res => {
+    setTimeout(() => {
+      res([
+        {
+          id: '1',
+          title: 'Go shopping'
+        },
+        {
+          id: '2',
+          title: 'Job interview'
+        },
+        {
+          id: '3',
+          title: 'Prepare homework'
+        }
+      ])
+    }, 100)
+  })
 
 function App() {
-  const [state, setState] = React.useState<{ id: string; title: string }[]>([]);
+  const [state, setState] = React.useState<{ id: string; title: string }[]>([])
 
   React.useEffect(() => {
-    (async () => {
-      var awaitedTodos = await todos();
-      for (var i = 0; i < awaitedTodos.length; i++) {
-        setState([...state, awaitedTodos[i]]);
+    ;(async () => {
+      const awaitedTodos = await todos()
+      for (let i = 0; i < awaitedTodos.length; i++) {
+        setState([...state, awaitedTodos[i]])
       }
     })()
   })
 
   return (
     <div>
-      {state.map((todo) => (
-        <Todo todo={todo} />
+      {state.map(todo => (
+        <Todo key={todo.id} todo={todo} />
       ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
