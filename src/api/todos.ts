19b7e763f6b3ff7015e42a16c1e0021ae225e152
Matúš Mrázik <matus.mrazik2@gmail.com>
2022-10-24 03:02:28 +0200

    Remove the mocked data, update README

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,29 +1,6 @@
 import axios from 'axios'
 import { DeleteTodoItemResponse, GetTodoItemResponse, GetTodosResponse, TodoItemEditableFields } from './types'
 
-// rework this into regular api call, feel free to use any open api
-export const todos = (): Promise<GetTodosResponse> =>
-  new Promise(res => {
-    setTimeout(() => {
-      res({
-        records: [
-          {
-            id: '1',
-            fields: { Title: 'Go shopping', Status: 'Todo' }
-          },
-          {
-            id: '2',
-            fields: { Title: 'Job interview', Status: 'Todo' }
-          },
-          {
-            id: '3',
-            fields: { Title: 'Prepare homework', Status: 'Todo' }
-          }
-        ]
-      })
-    }, 100)
-  })
-
 const API_KEY = ''
 
 export const BASE_TODOS_URL = 'https://api.airtable.com/v0/app42gVI0Kr0giHuX/Todos'
